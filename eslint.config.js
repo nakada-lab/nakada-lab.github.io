e4937ff4983686cfdeb9dc2e092b95f753ab473f
shallow90125 <shallow90125@gmail.com>
2024-08-24 00:25:23 +0900

    migrate to bun

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import pluginJs from '@eslint/js'
+import eslintPluginAstro from 'eslint-plugin-astro'
+import perfectionist from 'eslint-plugin-perfectionist'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+/** @type {import('typescript-eslint').Config}  */
+export default [
+  { files: ['**/*.{js,ts,astro}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  perfectionist.configs['recommended-natural'],
+  eslintPluginPrettierRecommended,
+]
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  plugins: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['hackgen', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+}
